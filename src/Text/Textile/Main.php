f2adc2759c5705e71c109c6d514e91307116f7a5
Robin Mehner <robin@coding-robin.de>
2010-07-04 15:57:53 +0200

    Initial PEAR2 package layout

---
+++ src/Text/Textile/Main.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * pear2\Text\Textile\Main
+ *
+ * PHP version 5
+ *
+ * @category  Text
+ * @package   PEAR2_Text_Textile
+ * @author    Robin Mehner <robin@coding-robin.de>
+ * @copyright 2010 Robin Mehner
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   GIT: $Id$
+ * @link      http://svn.php.net/repository/pear2/PEAR2_Text_Textile
+ */
+
+/**
+ * Main class for PEAR2_Text_Textile
+ *
+ * @category  Text
+ * @package   PEAR2_Text_Textile
+ * @author    Robin Mehner <robin@coding-robin.de
+ * @copyright 2010 Robin Mehner
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      http://svn.php.net/repository/pear2/PEAR2_Text_Textile
+ */
+namespace pear2\Text\Textile;
+class Main
+{
+}
